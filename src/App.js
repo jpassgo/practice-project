23feb354aa9748cbb53ec37ce8ecbce3036c4a35
jpassgo <jeffpascoe9@gmail.com>
2020-04-18 22:21:58 -0500

    Checking in.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import Todos from "./components/Todos";
 
-function App() {
+class App extends React.Component {
 
   state = {
     todos: [
@@ -10,14 +10,27 @@ function App() {
         id: 1, 
         title: 'Take out the trash',
         completed: false
+      },
+      {
+        id: 2, 
+        title: 'Clean up my room',
+        completed: false
+      },
+      {
+        id: 3, 
+        title: 'Feed the dog',
+        completed: true
       }
     ]
   }
-  return (
-    <div className="App">
-      <Todos />
-    </div>
-  );
+
+  render() {
+      return (
+        <div className="App">
+          <Todos todos={this.state.todos}/>
+        </div>
+      );
+    }
 }
 
 export default App;
--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import TodoItem from './TodoItem';
+import PropTypes from 'prop-types';
 
-function Todos() {
-  return (
-    <div className="Todos">
-      <h1></h1>
-    </div>
-  );
+class Todos extends React.Component {
+  render() {
+      return this.props.todos.map((todo) => (
+        <TodoItem key={todo.item} todo={todo}/>
+      ))
+  }
+}
+
+Todos.propTypes = {
+  todos: PropTypes.array.isRequired
 }
 
 export default Todos;
